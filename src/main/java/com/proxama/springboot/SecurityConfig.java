d1537567c82ae707a6f34d62f9aae833bdfb191a
Jon Vallet <jon.vallet@proxama.com>
2015-04-02 13:17:26 +0100

    Include all the resources

--- src/main/java/com/proxama/springboot/SecurityConfig.java
+++ src/main/java/com/proxama/springboot/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
+                    .antMatchers("/", "/home","/css/*","/js/*","/fonts/*").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
