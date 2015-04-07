c33bbd9fcb9e1c869f2cebffa56160978009d4a3
Jon Vallet <jon.vallet@proxama.com>
2015-04-07 10:50:11 +0100

    Added support to generate a deployable war

--- src/main/java/com/proxama/springboot/Application.java
+++ src/main/java/com/proxama/springboot/Application.java
@@ -2,9 +2,16 @@ package com.proxama.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
